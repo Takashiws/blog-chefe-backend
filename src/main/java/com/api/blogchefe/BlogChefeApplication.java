135affc9d4eeb1e11a3f55a24d550730fcff0bf4
Takashiws <takashi_ws@hotmail.com>
2023-05-18 00:11:44 -0300

    first commit

---
+++ src/main/java/com/api/blogchefe/BlogChefeApplication.java
@@ -0,0 +1,13 @@
+package com.api.blogchefe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogChefeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogChefeApplication.class, args);
+	}
+
+}
