c2f1ca45fb74a6ff8317d336c931c773c76552a1
Takashiws <takashi_ws@hotmail.com>
2023-05-18 01:25:25 -0300

    Comentario e usuario

--- src/main/java/com/api/blogchefe/models/Comentario.java
+++ src/main/java/com/api/blogchefe/models/Comentario.java
@@ -2,4 +2,40 @@ package com.api.blogchefe.models;
 
 public class Comentario {
 
+	private Long idComentario;
+	private String textoComentario;
+	private String nomePessoa;
+	private String emailPessoa;
+	private String avaliacao;
+	public Long getIdComentario() {
+		return idComentario;
+	}
+	public void setIdComentario(Long idComentario) {
+		this.idComentario = idComentario;
+	}
+	public String getTextoComentario() {
+		return textoComentario;
+	}
+	public void setTextoComentario(String textoComentario) {
+		this.textoComentario = textoComentario;
+	}
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+	public String getEmailPessoa() {
+		return emailPessoa;
+	}
+	public void setEmailPessoa(String emailPessoa) {
+		this.emailPessoa = emailPessoa;
+	}
+	public String getAvaliacao() {
+		return avaliacao;
+	}
+	public void setAvaliacao(String avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+	
 }
--- src/main/java/com/api/blogchefe/models/Usuario.java
+++ src/main/java/com/api/blogchefe/models/Usuario.java
@@ -2,4 +2,33 @@ package com.api.blogchefe.models;
 
 public class Usuario {
 
+	private Long idUsuario;
+	private String userName;
+	private String senha;
+	private String email;
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
